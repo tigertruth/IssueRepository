991094bb4796aad02e431fa637de81ba008802bf
Jin Lee <beomjin.lee0512@gmail.com>
2017-11-24 18:18:04 +0900

    analysis
    analysis

---
+++ analysis.py
@@ -0,0 +1,14 @@
+import csv
+
+#뉴스 데이터
+news = csv.reader(open("urls.txt","r",encoding="utf-8"), delimiter='\t')
+
+#주별 빈출 키워드
+#키워드 데려오기
+#주별 단어 스트링화
+#주별 빈출 단어 추출
+
+
+#월별 빈출 키워드
+#월별 단어 스트링화
+#월별 빈출 단어 추출
